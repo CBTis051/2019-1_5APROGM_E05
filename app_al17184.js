e44a8593e0d247f96751752c22e09625011fef76
Acosta Aguiluz Julio <al17184@cbtis051.edu.mx>
2019-12-02 10:29:18 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17184.js
+++ app_al17184.js
@@ -1,11 +1,14 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200,{'content-Type': 'application/json; charset=utf-8'});
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
     var objeto =  {
-        mensaje :   'Hola mundo Node JS Repo GIT - Acosta Aguiluz Julio \n'
+        mensaje : 'Hola mundo Node JS Repo GIT - Acosta Aguiluz Julio \n'
     };
 
-    res.end('json');
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+    
 }).listen(8081);
 console.log('Server running on port 8081');
